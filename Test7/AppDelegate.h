36e0623bdd08a8648acaccd53e390a044b104f38
OzekiSyunsuke <syun@syun.local>
2012-03-22 23:27:51 +0900

    Initial Commit

---
+++ Test7/AppDelegate.h
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.h
+//  Test7
+//
+//  Created by OzekiSyunsuke on 12/03/22.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class RootViewController;
+
+@interface AppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow			*window;
+	RootViewController	*viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
---
+++ Test7/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  Test7
+//
+//  Created by OzekiSyunsuke on 12/03/22.
+//  Copyright __MyCompanyName__ 2012年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
